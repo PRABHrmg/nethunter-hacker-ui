dd27d2d4974dcd741ef1f77b46fe464a5d7e6fe9
RMG <125893082+PRABHrmg@users.noreply.github.com>
2026-04-03 16:05:14 +0530

    Update system.sh

--- system.sh
+++ system.sh
@@ -14,20 +14,23 @@ cols=$(tput cols)
 if [ $cols -lt 60 ]; then
     cat ~/nethunter-hacker-ui/assets/small_logo.txt
 else
-    cat ~/nethunter-hacker-ui/assets/dragon.txt
+    while IFS= read -r line; do
+    printf "%*s\n" $(((${#line} + $(tput cols)) / 2)) "$line"
+done < ~/nethunter-hacker-ui/assets/dragon.txt
 fi
-sleep 1
-
+sleep 2
+clear
 echo ""
 echo "[+] System Booting..."
 sleep 0.5
 
 # Wipe down effect
 for i in {1..10}; do
-    echo "Loading modules..."
+    echo -ne "Loading modules [$i/10]\r"
     sleep 0.1
 done
-
+echo ""
+echo -e "\e[1;32m[+] System Booting...\e[0m"
 echo ""
 echo "OS       : Kali NetHunter"
 echo "Kernel   : $(uname -r)"
@@ -36,3 +39,5 @@ echo "kali㉿$USER"
 echo "CPU      : $(uname -m)"
 
 sleep 2
+echo -e "\e[1;31m[✔] System Ready\e[0m"
+sleep 1
